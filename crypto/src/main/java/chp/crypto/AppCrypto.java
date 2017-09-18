f23feb6b3afddba9405290dae29dfd8d6270be69
cperry <cperry@SWELP1104.hq.ghds.net>
2017-09-18 16:19:04 -0700

    First Commit

---
+++ crypto/src/main/java/chp/crypto/AppCrypto.java
@@ -0,0 +1,50 @@
+package chp.crypto;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ */
+public class AppCrypto 
+{
+    public static void main( String[] args )
+    {
+    	AppCrypto app = new AppCrypto();
+    	app.hash("Hello World!");
+    	
+    }
+
+	private void hash(String strToHash) {
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			byte[] byteHash = digest.digest(strToHash.getBytes(StandardCharsets.UTF_8));
+			System.out.println("Raw Bytes: "+byteHash);
+			
+			
+	        //convert the byte to hex format method 1
+	        StringBuffer sb = new StringBuffer();
+	        for (int i = 0; i < byteHash.length; i++) {
+	         sb.append(Integer.toString((byteHash[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+
+	        System.out.println("Hex format (method 1): " + sb.toString());
+			
+	      //convert the byte to hex format method 2
+	        StringBuffer hexString = new StringBuffer();
+	    	for (int i=0;i<byteHash.length;i++) {
+	    		String hex=Integer.toHexString(0xff & byteHash[i]);
+	   	     	if(hex.length()==1) hexString.append('0');
+	   	     	hexString.append(hex);
+	    	}
+	    	System.out.println("Hex format (method 2): " + hexString.toString());
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
