da90fe83a9f90d2ecf14dc9671d1314b3ab1d506
cperry <cperry@cperry-home-pc>
2017-10-15 09:37:13 -0700

    adding basic primitive examples

---
+++ crypto/src/main/java/chp/primitives/HexUtil.java
@@ -0,0 +1,22 @@
+package chp.primatives;
+
+public class HexUtil {	
+	
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray(); // All possible HEX symbols
+
+	public String bytesToHexStr(byte[] bytes) {
+
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			// The hex literal 0xFF is equal to int(255). Java represents int as 32 bits. It
+			// look like this in binary: 00000000 00000000 00000000 11111111
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];// Bit shift to the right by 4 bits. NOTE: The unsigned right shift
+												// operator ">>>" shifts a zero into the leftmost position
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+
+	}
+	
+}
